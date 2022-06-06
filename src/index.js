be8c25bacb916e098cb7966d78d56ae7e608a5b8
Lubov <lubaparfyonova@gmail.com>
2022-06-06 19:51:35 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -2,36 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
+import store from './stores';
 import {Provider} from 'react-redux';
 
-const initState = {
-  count: 0,
-  step: 1
-}
-
- const reducer = (state = initState, action) => {
-  switch(action.type){
-    case 'INCREMENT_COUNT':{
-     return {...state,
-      count: state.count + state.step}
-    }
-    case 'DECREMENT_COUNT':{
-      return {...state,
-       count: state.count - state.step}
-     }
-     case 'SET_STEP' : {
-       return {
-         ...state, 
-         step: action.value
-       }
-     }
-     default: {
-       return state
-     }
-  }
-}; 
-const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
